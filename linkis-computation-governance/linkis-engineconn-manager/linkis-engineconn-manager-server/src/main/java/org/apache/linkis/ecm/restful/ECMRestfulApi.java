ac9c39f58fd6b6e458befc3002c864e453c80c4b
v_kkhuang <420895376@qq.com>
2023-11-10 10:02:17 +0800

    code view  fix

--- linkis-computation-governance/linkis-engineconn-manager/linkis-engineconn-manager-server/src/main/java/org/apache/linkis/ecm/restful/ECMRestfulApi.java
+++ linkis-computation-governance/linkis-engineconn-manager/linkis-engineconn-manager-server/src/main/java/org/apache/linkis/ecm/restful/ECMRestfulApi.java
@@ -18,7 +18,6 @@
 package org.apache.linkis.ecm.restful;
 
 import org.apache.linkis.common.conf.Configuration;
-import org.apache.linkis.ecm.server.conf.ECMConfiguration;
 import org.apache.linkis.ecm.server.util.ECMUtils;
 import org.apache.linkis.server.Message;
 import org.apache.linkis.server.utils.ModuleUserUtils;
@@ -57,8 +56,10 @@ public class ECMRestfulApi {
 
   private final Logger logger = LoggerFactory.getLogger(ECMRestfulApi.class);
 
-  /***
-   * Reason for using the get method: Added gateway forwarding rules, which only support get requests
+  /**
+   * * Reason for using the get method: Added gateway forwarding rules, which only support get
+   * requests
+   *
    * @param req
    * @param response
    * @param emInstance
@@ -72,8 +73,16 @@ public class ECMRestfulApi {
       notes = "download engine log",
       response = Message.class)
   @ApiImplicitParams({
-    @ApiImplicitParam(name = "emInstance", required = true, dataType = "String",example = "xxx0002:9102"),
-    @ApiImplicitParam(name = "instance", required = true, dataType = "String",example = "xxx0002:35873"),
+    @ApiImplicitParam(
+        name = "emInstance",
+        required = true,
+        dataType = "String",
+        example = "xxx0002:9102"),
+    @ApiImplicitParam(
+        name = "instance",
+        required = true,
+        dataType = "String",
+        example = "xxx0002:35873"),
     @ApiImplicitParam(name = "logDirSuffix", required = true, dataType = "String"),
     @ApiImplicitParam(name = "logType", required = true, dataType = "String")
   })
