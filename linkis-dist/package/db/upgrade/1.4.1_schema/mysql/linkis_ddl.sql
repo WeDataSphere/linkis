1ab5eb9aaa881c4e1b9d6ba8a9104bba83703bb3
v_kkhuang <420895376@qq.com>
2023-11-16 20:25:16 +0800

    push department tenant code

--- linkis-dist/package/db/upgrade/1.4.1_schema/mysql/linkis_ddl.sql
+++ linkis-dist/package/db/upgrade/1.4.1_schema/mysql/linkis_ddl.sql
@@ -37,3 +37,20 @@ INSERT INTO linkis_ps_error_code (error_code,error_desc,error_regex,error_type) 
 ALTER TABLE  linkis_cg_tenant_label_config ADD COLUMN department VARCHAR(50) NOT NULL DEFAULT "*" COMMENT 'user department' AFTER `creator`;
 
 
+
+-- ----------------------------
+-- Table structure for linkis_org_user
+-- ----------------------------
+DROP TABLE IF EXISTS `linkis_org_user`;
+CREATE TABLE `linkis_cg_tenant_department_config` (
+  `id` int(20) NOT NULL AUTO_INCREMENT  COMMENT 'ID',
+  `creator` varchar(50) COLLATE utf8_bin NOT NULL  COMMENT '应用',
+  `department` varchar(50) COLLATE utf8_bin NOT NULL  COMMENT '部门名称',
+  `department_id` varchar(128) COLLATE utf8_bin NOT NULL COMMENT '部门ID',
+  `tenant_value` varchar(128) COLLATE utf8_bin NOT NULL  COMMENT '部门租户标签',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP  COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP  COMMENT '更新时间',
+  `bussiness_user` varchar(50) COLLATE utf8_bin NOT NULL  COMMENT '创建用户',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_user_creator` (`creator`,`department`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
