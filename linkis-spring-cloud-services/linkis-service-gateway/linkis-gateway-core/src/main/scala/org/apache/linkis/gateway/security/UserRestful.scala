18df85cc1705284bb8c1d7eed0ea9182aec6940f
“v_kkhuang” <420895376@qq.com>
2025-07-09 14:54:26 +0800

    禁止系统用户登录

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-core/src/main/scala/org/apache/linkis/gateway/security/UserRestful.scala
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-core/src/main/scala/org/apache/linkis/gateway/security/UserRestful.scala
@@ -289,6 +289,9 @@ abstract class UserPwdAbstractUserRestful extends AbstractUserRestful with Loggi
     } else if (StringUtils.isBlank(password)) {
       return Message.error("Password can not be blank(密码不能为空)！")
     }
+    if (userName.matches("hduser[0-9a-zA-Z_]+")) {
+      return Message.error("System users are prohibited from logging in（系统用户禁止登录）！")
+    }
     if (
         GatewayConfiguration.ADMIN_USER.getValue.equals(
           userName
