8894bf4d3cd1a6452d47a67be97fa400b1fc852d
“v_kkhuang” <420895376@qq.com>
2025-07-17 09:54:05 +0800

    Code optimization

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-core/src/main/scala/org/apache/linkis/gateway/config/GatewayConfiguration.scala
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-core/src/main/scala/org/apache/linkis/gateway/config/GatewayConfiguration.scala
@@ -126,6 +126,6 @@ object GatewayConfiguration {
 
   val ACCESS_CONTROL_USER_ENABLED = CommonVars("linkis.client.access.control.user.enable", false)
 
-  val PROHIBIT_LOGIN_SWITCH = CommonVars("linkis.system.prohibit.login.switch", false)
+  val PROHIBIT_LOGIN_SWITCH = CommonVars("linkis.system.user.prohibit.login.switch", false)
 
 }
