087e26e65d5919832dc21c1309ba3d6963765598
v-kkhuang <420895376@qq.com>
2026-03-17 19:09:42 +0800

    #AI commit# 开发阶段：优化spark参数获取

--- linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/executor/SparkEngineConnExecutor.scala
+++ linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/executor/SparkEngineConnExecutor.scala
@@ -329,26 +329,29 @@ abstract class SparkEngineConnExecutor(val sc: SparkContext, id: Long)
     var skippedParams = 0
     var successCount = 0
     var failCount = 0
-    logger.info(
-      s"Spark executor params setting begin"
-    )
-    this.asInstanceOf[SparkSqlExecutor].getSparkEngineSession.sparkSession.sessionState.conf.getAllConfs.foreach { case (key, value) =>
-      totalParams += 1
-      if (excludeParams.contains(key)) {
-        logger.info(
-          s"Spark executor params $key will be excluded and will not be set."
-        )
-        skippedParams += 1
-      } else {
-        Utils.tryCatch {
-          sc.setLocalProperty(key, value)
-          successCount += 1
-        } { case e: Exception =>
-          logger.warn(s"Failed to set spark param: $key, error: ${e.getMessage}", e)
-          failCount += 1
+    logger.info(s"Spark executor params setting begin")
+    this
+      .asInstanceOf[SparkSqlExecutor]
+      .getSparkEngineSession
+      .sparkSession
+      .sessionState
+      .conf
+      .getAllConfs
+      .foreach { case (key, value) =>
+        totalParams += 1
+        if (excludeParams.contains(key)) {
+          logger.info(s"Spark executor params $key will be excluded and will not be set.")
+          skippedParams += 1
+        } else {
+          Utils.tryCatch {
+            sc.setLocalProperty(key, value)
+            successCount += 1
+          } { case e: Exception =>
+            logger.warn(s"Failed to set spark param: $key, error: ${e.getMessage}", e)
+            failCount += 1
+          }
         }
       }
-    }
 
     logger.info(
       s"Spark executor params setting completed - total: $totalParams, " +
