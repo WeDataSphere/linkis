424c9a25666cf7f9fd1ee41cd461987997218a58
v-kkhuang <420895376@qq.com>
2026-06-01 17:23:57 +0800

    #AI commit# refactor: 日志打印优化 1.用户使用token禁止打印 2.用户执行code禁止打印 3.kill引擎新增引擎实例打印

--- linkis-engineconn-plugins/hive/src/main/scala/org/apache/linkis/engineplugin/hive/executor/HiveEngineConcurrentConnExecutor.scala
+++ linkis-engineconn-plugins/hive/src/main/scala/org/apache/linkis/engineplugin/hive/executor/HiveEngineConcurrentConnExecutor.scala
@@ -18,7 +18,7 @@
 package org.apache.linkis.engineplugin.hive.executor
 
 import org.apache.linkis.common.exception.ErrorException
-import org.apache.linkis.common.utils.{ByteTimeUtils, Logging, Utils}
+import org.apache.linkis.common.utils.{ByteTimeUtils, CodeUtils, Logging, Utils}
 import org.apache.linkis.engineconn.computation.executor.conf.ComputationExecutorConf
 import org.apache.linkis.engineconn.computation.executor.execute.{
   ConcurrentComputationExecutor,
@@ -27,6 +27,7 @@ import org.apache.linkis.engineconn.computation.executor.execute.{
 import org.apache.linkis.engineconn.core.EngineConnObject
 import org.apache.linkis.engineconn.executor.entity.{ConcurrentExecutor, ResourceFetchExecutor}
 import org.apache.linkis.engineplugin.hive.conf.{Counters, HiveEngineConfiguration}
+import org.apache.linkis.engineplugin.hive.conf.HiveEngineConfiguration.HIVE_TAG_USER_ENABLE
 import org.apache.linkis.engineplugin.hive.creation.HiveEngineConnFactory
 import org.apache.linkis.engineplugin.hive.cs.CSHiveHelper
 import org.apache.linkis.engineplugin.hive.errorcode.HiveErrorCodeSummary.COMPILE_HIVE_QUERY_ERROR
@@ -43,6 +44,7 @@ import org.apache.linkis.manager.common.entity.resource.{
 import org.apache.linkis.manager.common.protocol.resource.ResourceWithStatus
 import org.apache.linkis.manager.engineplugin.common.util.NodeResourceUtils
 import org.apache.linkis.manager.label.entity.Label
+import org.apache.linkis.manager.label.entity.engine.EngineType
 import org.apache.linkis.protocol.engine.JobProgressInfo
 import org.apache.linkis.scheduler.executer.{
   CompletedExecuteResponse,
@@ -134,13 +136,16 @@ class HiveEngineConcurrentConnExecutor(
       engineExecutorContext: EngineExecutionContext,
       code: String
   ): ExecuteResponse = {
-    LOG.info(s"HiveEngineConcurrentConnExecutor Ready to executeLine: $code")
+    LOG.info(s"HiveEngineConcurrentConnExecutor Ready to executeLine: ${CodeUtils
+      .maskCode(code, EngineType.HIVE.toString())}")
     val taskId: String = engineExecutorContext.getJobId.get
     CSHiveHelper.setContextIDInfoToHiveConf(engineExecutorContext, hiveConf)
 
     val realCode = code.trim()
 
-    LOG.info(s"hive client begins to run hql code:\n ${realCode.trim}")
+    LOG.info(
+      s"hive client begins to run hql code:\n ${CodeUtils.maskCode(realCode.trim, EngineType.HIVE.toString())}"
+    )
     val jobId = JobUtils.getJobIdFromMap(engineExecutorContext.getProperties)
     if (StringUtils.isNotBlank(jobId)) {
       LOG.info(s"set mapreduce.job.tags=LINKIS_$jobId")
@@ -224,11 +229,11 @@ class HiveEngineConcurrentConnExecutor(
             var compileRet = -1
             Utils.tryCatch {
               compileRet = driver.compile(realCode)
-              logger.info(
-                s"driver compile realCode : \n ${realCode} \n finished, status : ${compileRet}"
-              )
+              logger.info(s"driver compile realCode : \n ${CodeUtils
+                .maskCode(realCode, EngineType.HIVE.toString())} \n finished, status : ${compileRet}")
               if (0 != compileRet) {
-                logger.warn(s"compile realCode : \n ${realCode} \n error status : ${compileRet}")
+                logger.warn(s"compile realCode : \n ${CodeUtils
+                  .maskCode(realCode, EngineType.HIVE.toString())} \n error status : ${compileRet}")
                 throw HiveQueryFailedException(
                   COMPILE_HIVE_QUERY_ERROR.getErrorCode,
                   COMPILE_HIVE_QUERY_ERROR.getErrorDesc
