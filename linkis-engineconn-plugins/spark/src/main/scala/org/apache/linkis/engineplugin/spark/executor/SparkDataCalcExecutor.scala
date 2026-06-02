3164157db57a21a0574ce412a6b4829e695c0b10
v-kkhuang <420895376@qq.com>
2026-06-02 10:16:05 +0800

    #AI commit# refactor: 修复spark类型转换错误

--- linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/executor/SparkDataCalcExecutor.scala
+++ linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/executor/SparkDataCalcExecutor.scala
@@ -99,4 +99,6 @@ class SparkDataCalcExecutor(sparkEngineSession: SparkEngineSession, id: Long)
   override protected def getExecutorIdPreFix: String = "SparkDataCalcExecutor_"
 
   override protected def getKind: Kind = SparkDataCalc()
+
+  def getSparkEngineSession: SparkEngineSession = sparkEngineSession
 }
