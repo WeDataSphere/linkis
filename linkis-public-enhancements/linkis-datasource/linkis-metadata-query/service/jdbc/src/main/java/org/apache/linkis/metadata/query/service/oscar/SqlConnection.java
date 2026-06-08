8d9e86528932a11e1967153ec5145f63fb5d91b3
aiceflower <aiceflower@sina.com>
2026-06-08 19:48:56 +0800

    #AI commit add orsca ds

---
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/oscar/SqlConnection.java
@@ -0,0 +1,223 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.linkis.metadata.query.service.oscar;
+
+import org.apache.linkis.common.conf.CommonVars;
+import org.apache.linkis.common.utils.AESUtils;
+import org.apache.linkis.metadata.query.common.domain.MetaColumnInfo;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SqlConnection implements Closeable {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SqlConnection.class);
+
+  private static final CommonVars<String> SQL_DRIVER_CLASS =
+      CommonVars.apply("wds.linkis.server.mdm.service.oscar.driver", "com.oscar.jdbc.Driver");
+
+  private static final CommonVars<String> SQL_CONNECT_URL =
+      CommonVars.apply("wds.linkis.server.mdm.service.oscar.url", "jdbc:oscar://%s:%s/%s");
+
+  private Connection conn;
+
+  private ConnectMessage connectMessage;
+
+  public SqlConnection(
+      String host,
+      Integer port,
+      String username,
+      String password,
+      String database,
+      Map<String, Object> extraParams)
+      throws ClassNotFoundException, SQLException {
+    connectMessage = new ConnectMessage(host, port, username, password, extraParams);
+    conn = getDBConnection(connectMessage, database);
+    Statement statement = conn.createStatement();
+    statement.close();
+  }
+
+  public List<String> getAllDatabases() throws SQLException {
+    List<String> schemaNames = new ArrayList<>();
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SELECT USERNAME FROM ALL_USERS ORDER BY USERNAME");
+      while (rs.next()) {
+        schemaNames.add(rs.getString(1));
+      }
+    } finally {
+      closeResource(null, stmt, rs);
+    }
+    return schemaNames;
+  }
+
+  public List<String> getAllTables(String schema) throws SQLException {
+    List<String> tableNames = new ArrayList<>();
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      stmt = conn.createStatement();
+      rs =
+          stmt.executeQuery(
+              "SELECT TABLE_NAME FROM ALL_TABLES WHERE OWNER = '"
+                  + schema
+                  + "' ORDER BY TABLE_NAME");
+      while (rs.next()) {
+        tableNames.add(rs.getString(1));
+      }
+    } finally {
+      closeResource(null, stmt, rs);
+    }
+    return tableNames;
+  }
+
+  public List<MetaColumnInfo> getColumns(String schema, String table)
+      throws SQLException, ClassNotFoundException {
+    List<MetaColumnInfo> columns = new ArrayList<>();
+    String columnSql = "SELECT * FROM \"" + schema + "\".\"" + table + "\" WHERE 1 = 2";
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    ResultSetMetaData meta = null;
+    try {
+      List<String> primaryKeys = getPrimaryKeys(schema, table);
+      Map<String, String> columnCommentMap = getColumnComment(schema, table);
+      ps = conn.prepareStatement(columnSql);
+      rs = ps.executeQuery();
+      meta = rs.getMetaData();
+      int columnCount = meta.getColumnCount();
+      for (int i = 1; i < columnCount + 1; i++) {
+        MetaColumnInfo info = new MetaColumnInfo();
+        info.setIndex(i);
+        info.setLength(meta.getColumnDisplaySize(i));
+        info.setName(meta.getColumnName(i));
+        info.setType(meta.getColumnTypeName(i));
+        if (primaryKeys.contains(meta.getColumnName(i))) {
+          info.setPrimaryKey(true);
+        }
+        String colComment = columnCommentMap.get(meta.getColumnName(i));
+        if (StringUtils.isNotBlank(colComment)) {
+          info.setColComment(colComment);
+        } else {
+          info.setColComment(StringUtils.EMPTY);
+        }
+        columns.add(info);
+      }
+    } finally {
+      closeResource(null, ps, rs);
+    }
+    return columns;
+  }
+
+  private List<String> getPrimaryKeys(String schema, String table) throws SQLException {
+    ResultSet rs = null;
+    List<String> primaryKeys = new ArrayList<>();
+    DatabaseMetaData dbMeta = conn.getMetaData();
+    rs = dbMeta.getPrimaryKeys(null, schema, table);
+    while (rs.next()) {
+      primaryKeys.add(rs.getString("COLUMN_NAME"));
+    }
+    return primaryKeys;
+  }
+
+  private Map<String, String> getColumnComment(String schema, String table) throws SQLException {
+    ResultSet rs = null;
+    Map<String, String> columnComment = new HashMap<>();
+    DatabaseMetaData dbMeta = conn.getMetaData();
+    rs = dbMeta.getColumns(null, schema, table, "%");
+    while (rs.next()) {
+      columnComment.put(rs.getString("COLUMN_NAME"), rs.getString("REMARKS"));
+    }
+    return columnComment;
+  }
+
+  private void closeResource(Connection connection, Statement statement, ResultSet resultSet) {
+    try {
+      if (null != resultSet && !resultSet.isClosed()) {
+        resultSet.close();
+      }
+      if (null != statement && !statement.isClosed()) {
+        statement.close();
+      }
+      if (null != connection && !connection.isClosed()) {
+        connection.close();
+      }
+    } catch (SQLException e) {
+      LOG.warn("Fail to release resource [" + e.getMessage() + "]", e);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    closeResource(conn, null, null);
+  }
+
+  private Connection getDBConnection(ConnectMessage connectMessage, String database)
+      throws ClassNotFoundException, SQLException {
+    String extraParamString =
+        connectMessage.extraParams.entrySet().stream()
+            .map(e -> String.join("=", e.getKey(), String.valueOf(e.getValue())))
+            .collect(Collectors.joining("&"));
+    Class.forName(SQL_DRIVER_CLASS.getValue());
+    String url =
+        String.format(
+            SQL_CONNECT_URL.getValue(), connectMessage.host, connectMessage.port, database);
+    if (!connectMessage.extraParams.isEmpty()) {
+      url += "?" + extraParamString;
+    }
+    try {
+      Properties prop = new Properties();
+      prop.put("user", connectMessage.username);
+      prop.put("password", AESUtils.isDecryptByConf(connectMessage.password));
+      return DriverManager.getConnection(url, prop);
+    } catch (Exception e) {
+      LOG.error("Fail to create Oscar connection", e);
+      throw e;
+    }
+  }
+
+  private static class ConnectMessage {
+    private String host;
+    private Integer port;
+    private String username;
+    private String password;
+    private Map<String, Object> extraParams;
+
+    public ConnectMessage(
+        String host,
+        Integer port,
+        String username,
+        String password,
+        Map<String, Object> extraParams) {
+      this.host = host;
+      this.port = port;
+      this.username = username;
+      this.password = password;
+      this.extraParams = extraParams;
+    }
+  }
+}
