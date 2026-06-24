08f79c2dc5efb92cffe21d798f53a26e30ee5082
v-kkhuang <420895376@qq.com>
2026-06-24 10:10:12 +0800

    #AI commit# 开发阶段：修复oscar链接异常

--- linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/oscar/SqlConnection.java
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/oscar/SqlConnection.java
@@ -37,7 +37,7 @@ public class SqlConnection implements Closeable {
   private static final Logger LOG = LoggerFactory.getLogger(SqlConnection.class);
 
   private static final CommonVars<String> SQL_DRIVER_CLASS =
-      CommonVars.apply("wds.linkis.server.mdm.service.oscar.driver", "com.oscar.jdbc.Driver");
+      CommonVars.apply("wds.linkis.server.mdm.service.oscar.driver", "com.oscar.Driver");
 
   private static final CommonVars<String> SQL_CONNECT_URL =
       CommonVars.apply("wds.linkis.server.mdm.service.oscar.url", "jdbc:oscar://%s:%s/%s");
