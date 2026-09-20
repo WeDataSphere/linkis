9235b6aec046402dc0d363b1a1c914318d04e8a6
v_kkhuang <v_kkhuang@webank.com>
2026-09-20 09:43:11 +0800

    #AI commit# 开发阶段：content service增加日志打印

--- linkis-public-enhancements/linkis-cs-server/src/main/java/org/apache/linkis/cs/contextcache/cache/guava/ContextIDRemoveListener.java
+++ linkis-public-enhancements/linkis-cs-server/src/main/java/org/apache/linkis/cs/contextcache/cache/guava/ContextIDRemoveListener.java
@@ -19,6 +19,7 @@ package org.apache.linkis.cs.contextcache.cache.guava;
 
 import org.apache.linkis.cs.contextcache.cache.csid.ContextIDValue;
 import org.apache.linkis.cs.contextcache.cache.csid.impl.ContextIDValueImpl;
+import org.apache.linkis.cs.contextcache.metric.ContextIDMetric;
 import org.apache.linkis.cs.listener.ListenerBus.ContextAsyncListenerBus;
 import org.apache.linkis.cs.listener.event.impl.DefaultContextIDEvent;
 import org.apache.linkis.cs.listener.manager.imp.DefaultContextListenerManager;
@@ -27,6 +28,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.stereotype.Component;
 
+import com.google.common.cache.RemovalCause;
 import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalNotification;
 import org.slf4j.Logger;
@@ -49,8 +51,27 @@ public class ContextIDRemoveListener implements RemovalListener<String, ContextI
     if (StringUtils.isBlank(contextIDStr) || null == value || null == value.getContextID()) {
       return;
     }
+    RemovalCause cause = removalNotification.getCause();
+    long cachedMillis = -1L;
+    ContextIDMetric metric = value.getContextIDMetric();
+    if (null != metric && metric.getCachedTime() > 0) {
+      cachedMillis = System.currentTimeMillis() - metric.getCachedTime();
+    }
     listenerBus.removeListener((ContextIDValueImpl) value);
-    logger.info("Start to remove ContextID({}) from cache", contextIDStr);
+    if (RemovalCause.SIZE == cause) {
+      logger.warn(
+          "ContextID({}) is removed from cache, cause:{}, cachedMillis:{}, "
+              + "the cache reaches the max size, please consider tuning wds.linkis.cs.cache.size",
+          contextIDStr,
+          cause,
+          cachedMillis);
+    } else {
+      logger.info(
+          "ContextID({}) is removed from cache, cause:{}, cachedMillis:{}",
+          contextIDStr,
+          cause,
+          cachedMillis);
+    }
     DefaultContextIDEvent defaultContextIDEvent = new DefaultContextIDEvent();
     defaultContextIDEvent.setContextID(value.getContextKeyValueContext().getContextID());
     defaultContextIDEvent.setOperateType(DELETE);
